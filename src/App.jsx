b112b1f6aa6fff36dbd9751f9c8de21c9abe3d51
Idah Abubakar Isah <idahabubakar49@gmail.com>
2024-09-19 13:17:39 +0100

    Follow up page updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import Login from "./authentication/Login";
 import Drawer from "./components/Drawer";
 import Dashboard from "./pages/Dashboard";
 import Students from "./pages/Students";
-import Courses from "./pages/Courses";
 import EnquiryDetails from "./pages/EnquiryDetails";
+import Followups from "./pages/Followups";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -18,7 +18,7 @@ const router = createBrowserRouter(
     <Route path='drawer' element={<Drawer/>}>
     <Route path='dashboard' element={<Dashboard/>}/>
     <Route path='students' element={<Students/>}/>
-    <Route path='courses' element={<Courses/>}/>
+    <Route path='courses' element={<Followups/>}/>
     <Route path='enquiry-details' element={<EnquiryDetails/>}/>
 
     </Route>
